4dbfe5fc8c7a4e7c6bd755ba49e0c2cd0471e15e
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-03-13 16:45:58 -0400

    Append to the app for see the results

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
-import {Padre} from "./samples/react/fernando_erreras/tema9_hook/ejercicios/Padre.tsx";
+import {UseReducerApp} from "./samples/react/fernando_erreras/tema10_useReducer/UseReducerApp.tsx";
 
 function App() {
 
     return (
         <>
-            <Padre/>
+            <UseReducerApp/>
         </>
     )
 }
